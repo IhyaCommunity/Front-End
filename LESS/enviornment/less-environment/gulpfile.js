a0bf947ef550902ad1b2ec62d406998e91a98e0e
abuzarmushtaq <abuzarmushtaq2016@outlook.com>
2018-02-09 18:09:58 +0500

    browser-sync add

--- LESS/enviornment/less-environment/gulpfile.js
+++ LESS/enviornment/less-environment/gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const cleanCss = require('gulp-clean-css');
 const less = require('gulp-less');
+const BrowserSync = require('browser-sync').create();
+
 
 var lessDir = 'assets/less/**/*.less';
 
@@ -11,14 +13,25 @@ gulp.task('less', function () {
         .pipe(less())
         .pipe(cleanCss())
         .pipe(gulp.dest('assets/style'))
+        .pipe(BrowserSync.stream());
+});
+
+// live-reload
+
+gulp.task('live-reload',['less'], function () {
+    BrowserSync.init({
+        server: "./assets"
+    });
+    gulp.watch(lessDir, ['less']);
+    gulp.watch('./assets/*.html').on('change', BrowserSync.reload);
 });
 
 // Step-2 : Watch Task
 
-gulp.task('watch:less', function () {
-    gulp.watch(lessDir, ['less']);
-});
+// gulp.task('watch:less', function () {
+//     gulp.watch(lessDir, ['less']);
+// });
 
 // Step-3 : Create Default Task
 
-gulp.task('default', ['less', 'watch:less']);
+gulp.task('default', ['less', 'live-reload']);
