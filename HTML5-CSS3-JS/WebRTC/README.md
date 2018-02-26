413dca850063e1cd5f291899f3b65c5c0dee6c6a
Hannan Akbar <hannanakbar@outlook.com>
2018-02-27 04:25:29 +0500

    Add WebRTC

---
+++ HTML5-CSS3-JS/WebRTC/README.md
@@ -0,0 +1,5 @@
+https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API
+
+https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+https://developer.mozilla.org/en-US/docs/Web/API/Media_Streams_API
+https://github.com/webrtc/adapter
---
+++ HTML5-CSS3-JS/WebRTC/script.js
@@ -0,0 +1,33 @@
+if (navigator.mediaDevices.getUserMedia) {
+
+    var constraints = {
+        // video: {
+        //     width: { ideal: 1280 },
+        //     height: { ideal: 720 },
+        //     facingMode: "user"
+        // },
+        video: true,
+        audio: true
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        var video = document.querySelector('video');
+
+        try {
+            video.srcObject = stream;
+        } catch (error) {
+            video.src = URL.createObjectURL(stream);
+        }
+
+        // video.onloadedmetadata = function(e) {
+        //     video.play();
+        // };
+
+    }).catch((err)  =>  {
+        console.log(`${err.name}: ${err.message}`);
+    });
+
+}
+else {
+    console.log("User Media is not supported by your browser or device");
+}
