f268b2e4122495620f66750717a25a0c7d58bfc1
Hannan Akbar <hannanakbar@outlook.com>
2018-02-27 05:17:53 +0500

    Add Canvas

---
+++ HTML5-CSS3-JS/Canvas/README.md
@@ -0,0 +1,10 @@
+https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial
+
+http://origamijs.com/
+http://fabricjs.com/
+https://d3js.org/
+
+https://playcanvas.com/
+http://www.pixijs.com/
+https://www.createjs.com/
+https://github.com/mrdoob/three.js/
---
+++ HTML5-CSS3-JS/Canvas/script.js
@@ -0,0 +1,45 @@
+var canvas = document.getElementById('drawCanvas');
+var ctx = canvas.getContext('2d');
+
+// ctx.fillStyle = 'rgba(95, 202, 172, .5)';
+// ctx.fillRect(40, 40, 150, 150);
+
+var rect = new Path2D();
+rect.rect(20, 20, 150, 150);
+
+ctx.fillStyle = '#67217A';
+ctx.fill(rect);
+
+// ctx.strokeStyle = '#000';
+// ctx.lineWidth = 4;
+
+// ctx.beginPath();
+// ctx.moveTo(200, 150);
+// ctx.lineTo(350, 300);
+// ctx.closePath();
+// ctx.stroke();
+
+// var img = new Image();
+// img.addEventListener('load', () => {
+//     ctx.drawImage(img, 40, 40);
+
+//     ctx.shadowOffsetX = 2;
+//     ctx.shadowOffsetY = 2;
+//     ctx.shadowBlur = 2;
+//     ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+    
+//     ctx.font = '28px Arial';
+//     ctx.fillStyle = '#67217A';
+//     ctx.fillText('Some Text', 50, 50);
+
+// }, false);
+
+// img.src = 'hummingbird.jpg';
+
+
+// var link = document.getElementById('download');
+
+// link.addEventListener('click', () => {
+//     link.href = canvas.toDataURL();
+//     link.download = "image.png";
+// }, false);
