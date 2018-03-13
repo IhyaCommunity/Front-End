4304e5644f427d8fde0490f7e5b3bac3011590f3
Hannan Akbar <hannanakbar@hotmail.com>
2018-03-13 17:05:37 +0500

    Add fetch api ex

---
+++ HTML5-CSS3-JS/Fetch/script.js
@@ -0,0 +1,32 @@
+
+// GET
+
+const URL = 'https://httpbin.org/get?say=welcome';
+
+fetch(URL).then((response) => {
+  return response.json();
+}).then((data) => {
+  console.log(data);
+});
+
+// POST
+
+var data = {
+  name: 'Nameless',
+  message: 'Some message...'
+};
+
+var init = {
+  method: 'POST',
+  body: JSON.stringify(data),
+  headers: new Headers({
+    'Content-Type': 'application/json'
+  })
+};
+
+fetch('https://httpbin.org/post', init)
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch(error => console.error('Error:', error.message));
